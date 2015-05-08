5f8eb68f70d8cd9840c24cf31cb8de711905aafc
Higor Camporez <higorcamporez@hotmail.com>
2015-05-08 17:22:01 -0300

    separação do codigo html e php em aquivos diferentes e adicionando css

--- tarefas.php
+++ tarefas.php
@@ -1,44 +1,11 @@
-<!DOCTYPE html>
-<?php session_start(); ?>
-<html>
-<head>
-<meta charset="utf-8" />
-	<title>Gerenciador de Tarefas</title>
-	<link rel="stylesheet" href="tarefas.css" type="text/css">
-</head>
-<body>
-	<h1>Gerenciador de Tarefas</h1>
-	<!-- Aqui irá o restante do código -->
-	<form>
-		<fieldset>
-			<legend>Nova tarefa</legend>
-			<label>
-				Tarefa:
-				<input type="text" name="nome" />
-			</label>
-			<input type="submit" value="Cadastrar" />
-		</fieldset>
-	</form>
-
-		<?php
-if (isset($_GET['nome'])) {
-$_SESSION['lista_tarefas'][] = $_GET['nome'];
-}
-$lista_tarefas = array();
-if (isset($_SESSION['lista_tarefas'])) {
-$lista_tarefas = $_SESSION['lista_tarefas'];
-}
-?>		
-	
-	<table>
-		<tr>
-			<th>Tarefas</th>
-		</tr>
-		<?php foreach ($lista_tarefas as $tarefa) { ?>
-			<tr>
-				<td><?php echo $tarefa; ?> </td>
-			</tr>
-		<?php } ?>
-	</table>
-</body>
-</html>
+<?php
+	session_start();
+	if (isset($_GET['nome'])) {
+		$_SESSION['lista_tarefas'][] = $_GET['nome'];
+	}
+	if (isset($_SESSION['lista_tarefas'])) {
+		$lista_tarefas = $_SESSION['lista_tarefas'];
+	} else {
+		$lista_tarefas = array();
+	}
+include "template.php"; ?>
